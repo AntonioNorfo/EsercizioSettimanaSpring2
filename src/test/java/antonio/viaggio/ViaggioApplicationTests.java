b01db3ec3542e30d093fef7a0342697550126d8f
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-11-01 11:24:43 +0100

    creazione project Elaborazioen del progetto su drawsql

---
+++ src/test/java/antonio/viaggio/ViaggioApplicationTests.java
@@ -0,0 +1,13 @@
+package antonio.viaggio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaggioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
